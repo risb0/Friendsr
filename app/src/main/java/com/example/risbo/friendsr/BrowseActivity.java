93608fff1f68785672a6fb92d3cea44d5ffbaeca
risb0 <bponcet7@gmail.com>
2016-03-19 17:20:55 +0100

    Added Intents to pass names, images and descriptions through Activities.

--- app/src/main/java/com/example/risbo/friendsr/BrowseActivity.java
+++ app/src/main/java/com/example/risbo/friendsr/BrowseActivity.java
@@ -5,18 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class BrowseActivity extends AppCompatActivity {
+import java.util.HashMap;
 
+public class BrowseActivity extends AppCompatActivity {
+    private float rating;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        Intent intent = getIntent();
+        rating = intent.getFloatExtra("rating", 0);
+
     }
 
     public void onPhotoClick(View view) {
 
+        String friendsName = view.getTag().toString();
+
         Intent intent = new Intent(this,DetailsActivity.class);
+        intent.putExtra("friendsName",friendsName);
+        intent.putExtra("rating",rating);
         startActivity(intent);
+
+
+
 
     }
 
--- app/src/main/java/com/example/risbo/friendsr/DetailsActivity.java
+++ app/src/main/java/com/example/risbo/friendsr/DetailsActivity.java
@@ -1,13 +1,69 @@
 package com.example.risbo.friendsr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DetailsActivity extends AppCompatActivity {
+import stanford.androidlib.SimpleActivity;
 
+public class DetailsActivity extends SimpleActivity implements RatingBar.OnRatingBarChangeListener {
+
+    private String friendsName;
+    private float rating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+
+
+        ImageView profileImageView = (ImageView) findViewById(R.id.imageProfile);
+        TextView profileNameTextView = (TextView) findViewById(R.id.nameProfile);
+        TextView profileDescriptionTextView = (TextView) findViewById(R.id.descriptionProfile);
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+
+        Intent intent = getIntent();
+        friendsName = intent.getStringExtra("friendsName");
+        rating = intent.getFloatExtra("rating", 0);
+
+        ratingBar.setRating(rating);
+
+        ratingBar.setOnRatingBarChangeListener(this);
+
+
+
+
+
+        profileNameTextView.setText(friendsName);
+
+        int imageId = getResources().getIdentifier(friendsName.toLowerCase(),"drawable", getPackageName());
+        profileImageView.setImageResource(imageId);
+
+        int descriptionId = getResources().getIdentifier(friendsName.toLowerCase(),"raw",getPackageName());
+        String profileDescription = readFileText(descriptionId);
+        profileDescriptionTextView.setText(profileDescription);
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+        Intent intent = new Intent(this,BrowseActivity.class);
+        rating = ratingBar.getRating();
+        intent.putExtra("rating",rating);
+        startActivity(intent);
+
+        Toast.makeText(this, "You rated " + friendsName + " " + rating + " stars!", Toast.LENGTH_SHORT).show();
+
     }
 }
